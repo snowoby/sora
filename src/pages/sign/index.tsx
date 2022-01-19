c862a61a6546f9cc7362f2dbcee38d4327b89131
lynkas <cat@catting.net>
2022-01-20 00:01:03 +0800

    register and login

--- src/pages/sign/index.tsx
+++ src/pages/sign/index.tsx
@@ -1,20 +1,97 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, {
+  ChangeEvent,
+  ChangeEventHandler,
+  FormEvent,
+  useContext,
+  useState,
+} from "react";
+import { Link, Outlet } from "react-router-dom";
 import loginBackground from "@/assets/login_background.webp";
-import Panel from "@/pages/sign/Panel";
-const SignPage = () => {
+import UniversalContext from "@/context/UniversalContext";
+import { Button, Form } from "semantic-ui-react";
+import { LoginAPI } from "@/api/SignAPI";
+import log from "@/log";
+import Immutable from "immutable";
+
+type SignPageType = "login" | "register";
+
+const SignPage = ({ pageType }: { pageType: SignPageType }) => {
+  const universalConfig = useContext(UniversalContext);
+  const [form, setForm] = useState(
+    Immutable.Map({
+      email: "",
+      password: "",
+    })
+  );
+  const [submitting, setSubmitting] = useState(false);
+
+  const another = (value: SignPageType) =>
+    value === "login" ? "register" : "login";
+
+  const formSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const response = await LoginAPI(form);
+      //TODO
+    } catch (e) {
+      log.info(e);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  const inputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setForm((data) => data.set(event.target.name, event.target.value));
+  };
+
   return (
     <>
       <div
         style={{ backgroundImage: `url(${loginBackground})` }}
         className="h-screen w-screen bg-cover bg-bottom flex items-center justify-center"
       >
-        <Panel>
-          <Outlet />
-        </Panel>
+        <div className="m-0 max-w-md w-screen max-h-96 h-screen bg-red-50 rounded-lg p-2">
+          <h1 className="text-center text-4xl">{universalConfig.siteName}</h1>
+          <div className="mt-2">
+            <Form onSubmit={formSubmit}>
+              {["email", "password"].map((fieldName) => (
+                <Form.Field key={fieldName}>
+                  <label>{fieldName}</label>
+                  <input
+                    className="block"
+                    placeholder={fieldName}
+                    type={fieldName}
+                    name={fieldName}
+                    minLength={1}
+                    onChange={inputChange}
+                  />
+                </Form.Field>
+              ))}
+              <Button
+                inverted
+                fluid
+                color="blue"
+                className="block"
+                type="submit"
+                disabled={submitting}
+              >
+                {pageType}
+              </Button>
+            </Form>
+            <div className="w-full text-center">
+              <div className="my-2">-or-</div>
+              <Link to={`../${another(pageType)}`}>
+                -&gt;{another(pageType)}
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
 };
+
+const LoginButtonGroup = () => {};
 
 export default SignPage;
--- src/route.tsx
+++ src/route.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignPage from "@/pages/sign";
-import Login from "@/pages/sign/Login";
 
 const Guide = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="account" element={<SignPage />}>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={"register!"} />
+          <Route path="account">
+            <Route path="login" element={<SignPage pageType="login" />} />
+            <Route path="register" element={<SignPage pageType="register" />} />
           </Route>
         </Routes>
       </BrowserRouter>
