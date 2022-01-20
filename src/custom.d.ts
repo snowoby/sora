c12017e6be29ce741386e7676cb622c03b584fb2
lynkas <cat@catting.net>
2022-01-21 00:57:00 +0800

    material, again

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -3,3 +3,4 @@ declare module "*.svg" {
   export default content;
 }
 declare module "*.webp";
+declare module "*.scss";
--- src/pages/sign/index.tsx
+++ src/pages/sign/index.tsx
@@ -1,17 +1,12 @@
-import React, {
-  ChangeEvent,
-  ChangeEventHandler,
-  FormEvent,
-  useContext,
-  useState,
-} from "react";
-import { Link, Outlet } from "react-router-dom";
-import loginBackground from "@/assets/login_background.webp";
-import UniversalContext from "@/context/UniversalContext";
-import { Button, Form } from "semantic-ui-react";
-import { LoginAPI } from "@/api/SignAPI";
-import log from "@/log";
+import React, { ChangeEvent, FormEvent, useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import Immutable from "immutable";
+import { Button, Stack, TextField } from "@mui/material";
+import { Helmet } from "react-helmet";
+import log from "@/log";
+import UniversalContext from "@/context/UniversalContext";
+import loginBackground from "@/assets/login_background.webp";
+import { LoginAPI } from "@/api/SignAPI";
 
 type SignPageType = "login" | "register";
 
@@ -47,43 +42,56 @@ const SignPage = ({ pageType }: { pageType: SignPageType }) => {
 
   return (
     <>
+      <Helmet>
+        <title>{pageType}</title>
+      </Helmet>
       <div
         style={{ backgroundImage: `url(${loginBackground})` }}
         className="h-screen w-screen bg-cover bg-bottom flex items-center justify-center"
       >
-        <div className="m-0 max-w-md w-screen max-h-96 h-screen bg-red-50 rounded-lg p-2">
-          <h1 className="text-center text-4xl">{universalConfig.siteName}</h1>
-          <div className="mt-2">
-            <Form onSubmit={formSubmit}>
-              {["email", "password"].map((fieldName) => (
-                <Form.Field key={fieldName}>
-                  <label>{fieldName}</label>
-                  <input
+        <div className="group">
+          <div
+            className="m-20 max-w-md w-screen max-h-96 h-screen
+        bg-cover shadow-2xl backdrop-filter backdrop-blur-xl
+        bg-no-repeat bg-fixed rounded-lg p-4 group-hover:bg-white duration-200"
+          >
+            {
+              //TODO firefox
+            }
+            <h1 className="text-center text-4xl">{universalConfig.siteName}</h1>
+            <div className="mt-8">
+              <form onSubmit={formSubmit}>
+                <Stack spacing={3}>
+                  {["email", "password"].map((fieldName) => (
+                    <TextField
+                      key={fieldName}
+                      className="block"
+                      fullWidth
+                      label={fieldName}
+                      type={fieldName}
+                      name={fieldName}
+                      variant="filled"
+                      required={true}
+                      onChange={inputChange}
+                    />
+                  ))}
+                  <Button
                     className="block"
-                    placeholder={fieldName}
-                    type={fieldName}
-                    name={fieldName}
-                    minLength={1}
-                    onChange={inputChange}
-                  />
-                </Form.Field>
-              ))}
-              <Button
-                inverted
-                fluid
-                color="blue"
-                className="block"
-                type="submit"
-                disabled={submitting}
-              >
-                {pageType}
-              </Button>
-            </Form>
-            <div className="w-full text-center">
-              <div className="my-2">-or-</div>
-              <Link to={`../${another(pageType)}`}>
-                -&gt;{another(pageType)}
-              </Link>
+                    fullWidth
+                    variant="contained"
+                    type="submit"
+                    disabled={submitting}
+                  >
+                    {pageType}
+                  </Button>
+                </Stack>
+              </form>
+              <div className="w-full text-center">
+                <div className="my-2">-or-</div>
+                <Link to={`../${another(pageType)}`}>
+                  <Button>{another(pageType)}</Button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
@@ -91,7 +99,5 @@ const SignPage = ({ pageType }: { pageType: SignPageType }) => {
     </>
   );
 };
-
-const LoginButtonGroup = () => {};
 
 export default SignPage;
