36ea9f98f806d4dfad6fe24c0381639615cdc7ca
lynkas <cat@moki.cat>
2022-03-28 13:59:18 +0800

    remove worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
+  
 import App from "./App";
 import {
   createTheme,
@@ -35,7 +34,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to index results (for example: reportWebVitals(console.index))
